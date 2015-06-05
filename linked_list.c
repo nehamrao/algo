92b95927e5c7f9a71fc4182c17ac824051bb4236
Neha Rao <neha.m.rao.16@gmail.com>
2015-06-05 15:20:00 -0700

    Linked List (basic)

--- linked_list.c
+++ linked_list.c
@@ -3,21 +3,21 @@
 
 struct node {
     int    data;
-    struct node *p_node;
+    struct node *next;
 };
 
-void create_node(struct node *p_head, int data_a) {
+struct node* create_node(struct node *p_head, int data_a) {
      
     /* Allocate memory for the linked list*/
     p_head = malloc(sizeof(struct node));
 
     /* Add first node of linked list*/
     p_head->data = data_a;
-    p_head->p_node = NULL;
+    p_head->next = NULL;
 
     /* Check the values of the data*/
     printf("The data in first node is %d\n", p_head->data);
-    free(p_head);
+    return p_head;
 }
 
 /*
@@ -25,18 +25,18 @@ void create_node(struct node *p_head, int data_a) {
  * node creation required to 
  * form the linked list
  */
-void add_node(int num) {
+struct node* add_node(int num) {
     printf("CREATING a %d node linked list\n", num);
-    create_node(NULL,12);
+    struct node* p_head = create_node(NULL,12);
+    struct node* first_node = create_node(p_head, 13);
+    struct node* second_node = create_node(first_node, 78);
     
-    struct node *p_first;
-    create_node(p_first, 13);
-
-    struct node *p_second;
-    create_node(p_second, 78);
+    printf("Node1 [data]: %d, Node2 [data]: %d, Node3 [data]: %d\n", 
+           second_node->data, first_node->data, p_head->data);   
+    return second_node;
 }
 
 int main (void) {
-    add_node(3); 
+    add_node(3);
     return 0;
 }
