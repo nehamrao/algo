1395ab945beb0863fabba66019a59e63d98096f8
Neha Rao <neha.m.rao.16@gmail.com>
2016-04-11 23:52:36 -0700

    Add Digits O(1)

---
+++ add_digits2.c
@@ -0,0 +1,14 @@
+/*
+ *  Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+
+For example:
+
+Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
+
+Follow up:
+Could you do it without any loop/recursion in O(1) runtime? 
+ */
+
+int addDigits (int num) {
+    return (num % 9 == 0 && num != 0) ? 9 : (num %9);
+}
